0072ce1a4922012fc88503d9fd2092b8ee9166a5
Sohom Bose <44005253+sbose7@users.noreply.github.com>
2019-01-03 22:22:06 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,13 @@
 */
 
 $(function() {
-
+$('.video').click(function(){this.paused?this.play():this.pause();});
+$('.video').parent().click(function () {
+  if($(this).children(".video").get(0).paused){        $(this).children(".video").get(0).play();   $(this).children(".playpause").fadeOut();
+    }else{       $(this).children(".video").get(0).pause();
+  $(this).children(".playpause").fadeIn();
+    }
+});
 	// Vars.
 		var	$window = $(window),
 			$body = $('body'),
